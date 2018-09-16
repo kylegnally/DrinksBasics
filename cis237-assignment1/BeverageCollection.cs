1cc36b0b899937bb00593e99099871a2046580d1
Kyle Nally <kylegnally@gmail.com>
2018-09-15 21:17:40 -0400

    Add code to CSVProcessor. Add comments to BeverageCollection describing future purpose of class. Go to bed.

--- cis237-assignment1/BeverageCollection.cs
+++ cis237-assignment1/BeverageCollection.cs
@@ -8,5 +8,14 @@ namespace cis237_assignment1
 {
     class BeverageCollection
     {
+
+        // this class will initialize the collection size (passed into constructor from Main), 
+        // get the array for the entire collection, add new items, and search for a specific item.
+        // You will want to add an "item already present" flag by making the search dual-purpose;
+        // if it returns anything at all following the search by ID, there's already a beverage with that ID
+        // and so the user cannot add one with the chosen ID.
+
+        // search by ID first, THEN add the new item, ELSE error out. Burp from the UI.
+
     }
 }
--- cis237-assignment1/CSVProcessor.cs
+++ cis237-assignment1/CSVProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,60 @@ namespace cis237_assignment1
 {
     class CSVProcessor
     {
+        private StreamReader streamReader = null;
+
+        public bool ImportCSV(string pathToCSV, BeverageCollection[] beverages)
+        {
+            try
+            {
+                string csvLine;
+                streamReader = new StreamReader(pathToCSV);
+                int counter = 0;
+                while ((csvLine = streamReader.ReadLine()) != null)
+                {
+                    this.processCSVLine(csvLine, beverages, counter++);
+                }
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                // output the exception
+                Console.WriteLine(e.ToString());
+                Console.WriteLine();
+                Console.WriteLine(e.StackTrace);
+                // return false because we failed
+                return false;
+            }
+
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+            }
+        }
+
+        private void processCSVLine(string line, Beverage[] beverage, int index)
+        {
+            // declare am array of parts that will contain the results of
+            // splitting the string
+            string[] parts = line.Split(',');
+
+            // assign each part to a variable
+            string id = parts[0];
+            string desc = parts[1];
+            string pack = parts[2];
+            //string price = parts[3];
+            decimal price = decimal.Parse(parts[3]);
+
+            bool active = bool.Parse(parts[4]);
+
+            // write the method for this into the beverage collection class. This is what
+            // will initially populate the collection with the beverages when the list is loaded
+            // for the first time
+            //beverageCollection.AddNewItem(id, desc, pack, price, active);
+        }
     }
 }
